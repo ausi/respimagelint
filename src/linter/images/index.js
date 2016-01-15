7b06e0f2c257053bc8f61c7351674057642f429c
Martin Auswöger <martin@auswoeger.com>
2016-01-15 16:36:10 +0100

    Better order for image linters

--- src/linter/images/index.js
+++ src/linter/images/index.js
@@ -4,7 +4,7 @@ import differentRatio from './differentRatio';
 
 export default function(image) {
 	allSources(image).forEach(item => {
-		differentContent(item, image.images);
 		differentRatio(item, image.images);
+		differentContent(item, image.images);
 	});
 }
