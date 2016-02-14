8f99dd2b482d5d1db5ea11221e5c23954613b370
Martin Auswöger <martin@auswoeger.com>
2016-02-14 16:01:22 +0100

    Split bookmarklet into separate file

--- gulp/pages.js
+++ gulp/pages.js
@@ -1,9 +1,19 @@
 import gulp from 'gulp';
+import fs from 'fs';
+import path from 'path';
 import eslint from 'gulp-eslint';
 import livereload from 'gulp-livereload';
+import replace from 'gulp-replace';
+import uglify from 'uglify-js';
 
 gulp.task('pages', () =>
 	gulp.src(['./pages/**/*.html'])
+		.pipe(replace(
+			'{{> bookmarklet}}',
+			'javascript:(function(){' + encodeURI(uglify.minify(
+				path.join(__dirname, '..', 'src', 'bookmarklet.js')
+			).code) + '})()'
+		))
 		.pipe(gulp.dest('./dist'))
 		.pipe(livereload())
 );
---
+++ src/bookmarklet.js
@@ -0,0 +1,10 @@
+(function(){
+
+var script = document.createElement('script');
+script.id = 'respimagelint-script';
+script.type = 'text/javascript';
+script.src = 'https://ausi.github.io/respimagelint/collector.js?' + Date.now();
+
+document.body.appendChild(script);
+
+})();
