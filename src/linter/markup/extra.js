0cd3f6228cbefcdba78a11b8887c7f971211a15d
Martin Auswöger <martin@auswoeger.com>
2016-04-12 23:23:34 +0200

    Allow script and template elements inside picture, fixes #6

--- src/linter/markup/extra.js
+++ src/linter/markup/extra.js
@@ -1,10 +1,19 @@
 import error from '../../util/error';
 
+// Valid child elements for `<picture>` according to
+// https://html.spec.whatwg.org/multipage/embedded-content.html#the-picture-element
+const validChildren = [
+	'source',
+	'img',
+	'script',
+	'template',
+];
+
 export default function(image) {
 	const badTags = [];
 	if (image.markup.tag === 'picture') {
 		image.markup.children.forEach(child => {
-			if (child.tag !== 'source' && child.tag !== 'img' && badTags.indexOf(child.tag) === -1) {
+			if (validChildren.indexOf(child.tag) === -1 && badTags.indexOf(child.tag) === -1) {
 				badTags.push(child.tag);
 			}
 		});
